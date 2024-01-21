64299732f8a81a7ea4c6287e332c8aacd4e29e1b
cg <cg@cryptng.com>
2024-01-21 18:31:26 +0100

    Fix: ux

--- sources/genuine-captcha-web/ember-cli-build.js
+++ sources/genuine-captcha-web/ember-cli-build.js
@@ -15,6 +15,7 @@ module.exports = function (defaults) {
     }
   });
 
+ // app.import('./public/scripts/ai-term.js');
   // Use `app.import` to add additional libraries to the generated
   // output files.
   //
